b8ca2d4b85c15509cbb820c8e498042756ffefa0
Takashi-Na <69133170+Takashi-Na@users.noreply.github.com>
2021-06-19 20:02:39 +0900

    fix js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../maps")
-// require("../get_places")
-// require("../show_place")
+// require("../search_ranking")
+require("../get_place")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
